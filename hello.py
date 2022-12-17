6c27efb0042c423e5b7329e4b77d521d2a1c4b81
Vladislav <xlebywek2001@gmail.com>
2022-12-17 18:11:45 +0500

    Изменил форму flask

--- hello.py
+++ hello.py
@@ -5,14 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-		return "<a href='/1'>ON</a><a href='/2'>OFF</a>"
+		result = "<a href='/1'>ON</a><br></br>"
+		result += "<a href='/2'>OFF</a>"
+		return result
 
 @app.route("/1")
 def on():
 		os.system("echo 1 > /sys/class/gpio/gpio199/value")
-		return "ON"
+		return "Lamp is ON<br></br><a href='/'>Back to main page</a>"
 		
 @app.route("/2")
 def off():
 		os.system("echo 0 > /sys/class/gpio/gpio199/value")
-		return "OFF"
+		return "Lamp is OFF<br></br><a href='/'>Back to main page</a>"
